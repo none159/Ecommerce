e559479985bcb3f9bf2d6efa04ed3d26288ed0aa
none159 <noneenone019#gmail.com>
2024-03-28 02:13:21 +0000

    first commit

--- Server/api/index.cjs
+++ Server/api/index.cjs
@@ -1,7 +1,7 @@
 
 const express=require("express")
 const items = require("../data/products.json");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 const cors = require("cors");
 const connectDatabase = require("../config/mongodb.cjs");
 const ImportData = require("../data/dataimport.cjs");
