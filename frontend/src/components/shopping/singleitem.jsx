fc9701660dd3b44c27d561d33d7308c030f7a1c6
none159 <noneenone019#gmail.com>
2024-04-06 11:18:59 +0000

    first commit

--- frontend/src/components/shopping/singleitem.jsx
+++ frontend/src/components/shopping/singleitem.jsx
@@ -7,17 +7,18 @@ const Singleitem = (props)=>{
         localStorage.setItem("cart",JSON.stringify(JSON.parse(localStorage.getItem("cart")).filter((item)=>item.id!=id)))
     }
     return(
-      <Link style={{ textDecoration: 'none',width:"fit-content" }} to={`/fulldetails?id=${props.item.id}&categorie=${props.item.categorie}`}><div style={{paddingBottom:props.item.quantity!=undefined?"220px":""}}className="item" key={props.item.id}>
+     <div style={{paddingBottom:props.item.quantity!=undefined?"220px":""}}className="item" key={props.item.id}>
              <button className="close-button"  onClick={(e) => {
           e.stopPropagation(); 
           handleDelete(props.item.id);
         }}>X</button>
+         <Link style={{ textDecoration: 'none',width:"fit-content" }} to={`/fulldetails?id=${props.item.id}&categorie=${props.item.categorie}`}>
             <img loading="lazy" src={props.item.img} ></img>
             <h2>{props.item.name}</h2>
             <h3>Price: <span>{props.item.price}</span></h3>
             {props.item.size !=undefined&& props.item.quantity != undefined? <><h3>Size : <span>{props.item.size}</span></h3><h3>Quantity : <span>{props.item.quantity}</span></h3></>:""}
-        </div>
         </Link>
+        </div>
     )
 }
 export default Singleitem
