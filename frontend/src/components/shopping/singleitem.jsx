bf9f7e802d4e9c8570e4d836e8a32576e2bf5730
none159 <noneenone019#gmail.com>
2024-04-06 11:32:42 +0000

    first commit

--- frontend/src/components/shopping/singleitem.jsx
+++ frontend/src/components/shopping/singleitem.jsx
@@ -3,21 +3,22 @@ import { Link } from "react-router-dom";
 import "../../assets/items.css"
 
 const Singleitem = (props)=>{
+    const [items, setItems] = useState(props.items);
     const handleDelete=(id)=>{
         localStorage.setItem("cart",JSON.stringify(JSON.parse(localStorage.getItem("cart")).filter((item)=>item.id!=id)))
-        props.onDelete(id)
+        setItems(JSON.parse(localStorage.getItem("cart")))
     }
     return(
-     <div style={{paddingBottom:props.item.quantity!=undefined?"220px":""}}className="item" key={props.item.id}>
-             <button className="close-button"  onClick={(e) => {
+     <div style={{paddingBottom:items.quantity!=undefined?"220px":""}}className="item" key={items.id}>
+             {items.id?<button className="close-button"  onClick={(e) => {
           e.stopPropagation(); 
-          handleDelete(props.item.id);
-        }}>X</button>
-         <Link style={{ textDecoration: 'none',width:"fit-content" }} to={`/fulldetails?id=${props.item.id}&categorie=${props.item.categorie}`}>
-            <img loading="lazy" src={props.item.img} ></img>
-            <h2>{props.item.name}</h2>
-            <h3>Price: <span>{props.item.price}</span></h3>
-            {props.item.size !=undefined&& props.item.quantity != undefined? <><h3>Size : <span>{props.item.size}</span></h3><h3>Quantity : <span>{props.item.quantity}</span></h3></>:""}
+          handleDelete(items.id);
+        }}>X</button>:""}
+         <Link style={{ textDecoration: 'none',width:"fit-content" }} to={`/fulldetails?id=${items.id}&categorie=${items.categorie}`}>
+            <img loading="lazy" src={items.img} ></img>
+            <h2>{items.name}</h2>
+            <h3>Price: <span>{items.price}</span></h3>
+            {items.size !=undefined&& items.quantity != undefined? <><h3>Size : <span>{items.size}</span></h3><h3>Quantity : <span>{items.quantity}</span></h3></>:""}
         </Link>
         </div>
     )
