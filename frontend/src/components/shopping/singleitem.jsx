092714bacba72aa7e8cad39d2e742cf3f24b7715
none159 <noneenone019#gmail.com>
2024-04-06 11:45:41 +0000

    first commit

--- frontend/src/components/shopping/singleitem.jsx
+++ frontend/src/components/shopping/singleitem.jsx
@@ -3,7 +3,7 @@ import { Link ,useState} from "react-router-dom";
 import "../../assets/items.css"
 
 const Singleitem = (props)=>{
-    const [items, setItems] = useState(props.items);
+    const [items, setItems] = useState(props.item);
     const handleDelete=(id)=>{
         localStorage.setItem("cart",JSON.stringify(JSON.parse(localStorage.getItem("cart")).filter((item)=>item.id!=id)))
         setItems(JSON.parse(localStorage.getItem("cart")))
