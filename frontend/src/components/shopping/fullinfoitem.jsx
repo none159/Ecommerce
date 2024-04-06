75d3a1071f2120c7f33bd2e434ff0f188e6e161f
none159 <noneenone019#gmail.com>
2024-04-06 01:54:41 +0000

    first commit

--- frontend/src/components/shopping/fullinfoitem.jsx
+++ frontend/src/components/shopping/fullinfoitem.jsx
@@ -31,8 +31,8 @@ const Fullinfoitem =()=>{
     }
     const addtocart=async()=>{
  
-        if(size != "" && quantity!=0 && !res && !JSON.parse(localStorage.getItem("cart")).find((p)=>p.id==id)){
-            if (JSON.parse(localStorage.getItem("cart")!=undefined)){
+        if(size != "" && quantity!=0 && !res){
+            if (JSON.parse(localStorage.getItem("cart")!=undefined) && !JSON.parse(localStorage.getItem("cart"))?.find((p)=>p.id==id)){
                 const cartitems = JSON.parse(localStorage.getItem("cart"))
                 console.log(cartitems)
                 cartitems.push({
