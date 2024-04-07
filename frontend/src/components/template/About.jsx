4c859e8e694cbafd17cfad9b9df36739c467cf26
none159 <noneenone019#gmail.com>
2024-04-07 12:59:33 +0000

    first commit

--- frontend/src/components/template/About.jsx
+++ frontend/src/components/template/About.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../../assets/about.css"
+import Navbar from "../home/navbar";
+import Footer from "../home/footer";
 const About = ()=>{
     return(
         <>
+        <Navbar/>
         <section className="about-section">
         <h2>About</h2>
         <h3>In this MERN eCommerce project, I've built a user-friendly platform. It features easy product browsing, a simple shopping cart, and a way to save favorites—all securely stored. Users can sign up and make payments safely, making shopping hassle-free.</h3>
         </section>
+        <Footer/>
         </>
     )
 }
